10de853a73e57ece8e2955fc0c5bb72eb1989e62
Anthony Ettinger <anthony@chovy.com>
2023-10-10 01:19:16 -0700

    build

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,24 @@
-#!/bin/env sh
+#!/bin/sh
 
-eas build --profile development --platform all
+mode=$1
+platform=$2
+
+if [ "$mode" == "production" ]; then
+    echo "Building in production mode"
+    if [ "$platform" == "android" ]; then
+  		eas build --profile production --platform android
+    elif [ "$platform" == "ios" ]; then
+  		eas build --profile production --platform ios
+    else
+  		eas build --profile production --platform all
+    fi
+else
+    echo "Building in development mode"
+    if [ "$platform" == "android" ]; then
+  		eas build --profile development --platform android
+    elif [ "$platform" == "ios" ]; then
+  		eas build --profile development --platform ios
+    else
+  		eas build --profile development --platform all
+    fi
+fi
